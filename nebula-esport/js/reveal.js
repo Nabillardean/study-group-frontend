e25ae78c8a5902969fd45d15d0c0bb006810766c
Nabil Ardean Akbar <nabilardean0@gmail.com>
2026-02-06 04:07:48 +0700

    Add files via upload

---
+++ nebula-esport/js/reveal.js
@@ -0,0 +1,17 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 150;
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
